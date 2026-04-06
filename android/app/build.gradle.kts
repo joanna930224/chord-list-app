7a3b04dbfef1795886602320a4d0656a3d3e4c8b
ANNA <annavu0224@gmail.com>
2026-04-06 14:52:10 +0900

    feat : 앱 이름(App Name) 설정 (#7)

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -28,6 +28,7 @@ android {
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
         versionName = flutter.versionName
+        resValue("string", "app_name", "ChordMap")
     }
 
     buildTypes {
